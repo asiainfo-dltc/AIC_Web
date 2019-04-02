367cd451c9c4c211f6976417bb901a3f4ccad172
hexin5_asiainfo <limeng5@asiainfo.com>
2019-04-02 15:46:01 +0800

    修改pulicpath

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -4,6 +4,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const merge = require('webpack-merge');
 const webpackBaseConfig = require('./webpack.base.config.js');
 const fs = require('fs');
+const path=require('path');
 
 fs.open('./src/config/env.js', 'w', function(err, fd) {
     const buf = 'export default "production";';
@@ -12,7 +13,7 @@ fs.open('./src/config/env.js', 'w', function(err, fd) {
 
 module.exports = merge(webpackBaseConfig, {
     output: {
-        publicPath: '/dist/',
+        publicPath: '/',
         filename: '[name].[hash].js',
         chunkFilename: '[name].[hash].chunk.js'
     },
@@ -30,13 +31,13 @@ module.exports = merge(webpackBaseConfig, {
                 NODE_ENV: '"production"'
             }
         }),
-        new webpack.optimize.UglifyJsPlugin({
+      /*  new webpack.optimize.UglifyJsPlugin({
             compress: {
                 warnings: false
             }
-        }),
+        }),*/
         new HtmlWebpackPlugin({
-            filename: '../index_prod.html',
+            filename: './index_prod.html',
             template: './src/template/index.ejs',
             inject: false
         })
